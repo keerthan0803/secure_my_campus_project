ac7dacae99fac5934480e52dfe60f42cdf0b8a76
PENTAM KEERTHAN <23eg106b48@anurag.edu.in>
2025-08-22 11:14:25 +0530

    Add files via upload

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'KEERTHAN' });
+  res.render('index', { title: 'Secure My Campus' });
 });
 
 module.exports = router;
